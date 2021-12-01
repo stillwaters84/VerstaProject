28b2c6abc9ee160f9d37eeb1d89a62be55631348
stillwaters84 <egartsev9@gmail.com>
2021-12-01 11:50:12 +0300

    Изменено оформление стартовой страницы и добавлены атрибуты валидации полей модели

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,17 @@ namespace TestTask1.Models
     public class Order
     {
         public int id { get; set; }
+        [Required]
         public string SenderCity { get; set; }
+        [Required]
         public string SenderAddress { get; set; }
+        [Required]
         public string RecieverCity { get; set; }
+        [Required]
         public string RecieverAddress { get; set; }
+        [Required]
         public float Weight { get; set; }
+        [Required]
         public DateTime Date { get; set; }
     }
 }
